03f26eb370d2bed9e29e7020ed0f7ddd33411d91
ReisG <romanryabcev2006@gmail.com>
2025-11-06 16:37:38 +0300

    template for app (ping prints pong)

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"Hello" : "World"}
+@app.get("/ping")
+def ping_pong():
+    return "pong"
 
-@app.get("/items/{item_id}")
-def read_item(item_id : int, q: Union[str, None] = None):
-    return {"item_id" : item_id, "q" : q}
+@app.get('/visits')
+def print_visits():
+    return 2
