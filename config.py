3dd19b40aa6c713c27cf75bf2a3e291aee4ebf6b
ReisG <romanryabcev2006@gmail.com>
2025-11-06 18:55:32 +0300

    started to make requests to database

---
+++ config.py
@@ -0,0 +1,4 @@
+DBUSER = "root"
+DBPASSWORD = "password"
+DBADDR = "localhost"
+DBPORT = "3306"
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 #! ./venv/bin/fastapi dev
-from typing import Union
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi import Request
 
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+import config
+
+db_url = "mysql://{DBUSER}:{DBPASSWORD}@{DBADDR}:{DBPORT}"
+engine = None
+
+class Ping(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    ip: str
+
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    #connect to db
+    global engine
+    engine = create_engine(db_url, echo=True)
 
 @app.get("/ping")
 def ping_pong(request : Request):
     ip = request.client.host
-    print(ip)
+    session = Session(engine)
+
+    ping = Ping(ip=ip)
+    session.add(ping)
+    session.commit()
+
+    session.close()
     return "pong"
 
 @app.get('/visits')
